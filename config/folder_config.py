ea013bd98d184033dbb4cd2571acd5a83661b0e9
Stefan-Bojilov <bojilov.stefan@gmail.com>
2025-07-06 16:14:50 +0200

    chore: remove junk code

--- config/folder_config.py
+++ config/folder_config.py
@@ -41,12 +41,3 @@ class DataFolder(BaseModel):
             figures_dir=proj_root / "reports" / "figures",
         )
 
-
-# Optional: patch loguru to work well with tqdm
-try:
-    from tqdm import tqdm
-
-    logger.remove(0)
-    logger.add(lambda msg: tqdm.write(msg, end=""), colorize=True)
-except ModuleNotFoundError:
-    pass
